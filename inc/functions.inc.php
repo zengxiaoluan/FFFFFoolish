0e8b8a704effa0788e27ae6f9f514059072c7881
zhx <zhx@addcn.com>
2018-07-01 17:18:39 +0800

    remove ads-widgets

--- inc/functions.inc.php
+++ inc/functions.inc.php
@@ -6,7 +6,7 @@ function foolish_widgets_init() {
       'name' => __( 'Ads Sidebar', 'foolish' ),
       'id' => 'ads-sidebar',
       'description' => __( 'Widgets in this area will be shown on all posts and pages.', 'foolish' ),
-      'before_widget' => '<span id="%1$s" class="ads-widget %2$s">',
+      'before_widget' => '<span id="%1$s" class="%2$s">',
       'after_widget'  => '</span>',
       'before_title'  => '<h2 class="widgettitle">',
       'after_title'   => '</h2>',
